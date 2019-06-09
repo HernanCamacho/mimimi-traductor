34441393b4fbf48a7c2e93e01142b732fdfd51bf
Hernan Camacho <hcamacho@ucol.mx>
2019-06-09 18:51:47 -0500

    Created Mimimi traductor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mimimi';
+  public mimimi;
+  public content;
+  public separador;
+  public vocales;
+
+  constructor(){
+      this.mimimi = 'Mimimi';
+      this.content = '';
+      this.separador = "";
+      this.vocales = ['A', 'E', 'O', 'U'];
+  }
+
+  change(text){
+      this.content = document.getElementById("text").value;
+      this.content = this.content.toUpperCase();
+      this.content = this.content.split(this.separador);
+      for(let i = 0; i < this.content.length; i++){
+          for(let j = 0; j < 4; j++){
+              if(this.content[i] == this.vocales[j]){
+                  this.content[i] = "I";
+              }
+          }
+      }
+
+      console.log(this.content);
+  }
+
 }
